7332a99a0cfc85efc2f61647eaf9daad161d6e99
Shashwat Dubey <krishnadubey0537@gmail.com>
2026-03-16 09:59:40 +0530

    Delete customer-service directory

--- customer-service/src/main/java/com/smartrecharge/customerservice/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.smartrecharge.customerservice.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
-
--- customer-service/src/main/java/com/smartrecharge/customerservice/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.smartrecharge.customerservice.repository;
-
-import com.smartrecharge.customerservice.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
-
